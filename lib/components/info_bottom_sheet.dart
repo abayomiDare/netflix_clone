19b9889c414c5be3234c4caf69d562626c3ac6c7
abayomiDare <01.abayomidare@gmail.com>
2023-01-13 12:05:31 +0100

    retouch custom app bar

--- lib/components/info_bottom_sheet.dart
+++ lib/components/info_bottom_sheet.dart
@@ -6,8 +6,7 @@ class InfoBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
-      padding: const EdgeInsets.all(20.0),
+      height: 280,
       decoration: BoxDecoration(
         color: Colors.grey.shade800,
         borderRadius: const BorderRadius.only(
@@ -18,18 +17,44 @@ class InfoBottomSheet extends StatelessWidget {
       child: Column(
         children: [
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Expanded(
                 child: Container(
+                  height: 150,
+                  width: 50,
                   decoration: const BoxDecoration(
-                      image: DecorationImage(
-                    image: AssetImage(''),
-                  )),
+                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    image: DecorationImage(
+                        image: AssetImage('images/banner.webp'),
+                        fit: BoxFit.cover),
+                  ),
                 ),
               ),
               Expanded(
-                flex: 5,
-                child: Container(),
+                flex: 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Text('Movie Title'),
+                    const SizedBox(height: 3),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      // mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Flexible(child: Text('2018')),
+                        Flexible(child: Text('13+')),
+                        Flexible(flex:2,child: Text('19 Episodes')),
+                        Spacer(),
+                      ],
+                    ),
+                    const SizedBox(height: 3),
+                    const Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+                  ],
+                ),
               ),
             ],
           )
