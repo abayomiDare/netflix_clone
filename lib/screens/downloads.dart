5164255e2cf02e77ea845555c81a0e4b6666fa84
abayomiDare <01.abayomidare@gmail.com>
2023-01-17 05:25:32 +0100

    config quick and laugh screen

--- lib/screens/downloads.dart
+++ lib/screens/downloads.dart
@@ -5,8 +5,93 @@ class DownloadScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.lightBlueAccent,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: Row(
+            children: const [
+              Icon(Icons.settings,color: Colors.grey,),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                "Smart Downloads",
+                style: TextStyle(color: Colors.grey),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 60,
+        ),
+        Column(
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.grey.withOpacity(0.2)),
+              child: Center(
+                child: Icon(
+                  Icons.file_download,
+                  size: 80,
+                  color: Colors.grey.withOpacity(0.3),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              "Never be without Netflix",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 40, right: 40),
+              child: Text(
+                "Download shows and movies so you'll never be without something to watch even when you're offline",
+                style: TextStyle(
+                    height: 1.5, fontSize: 16, fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 50, right: 50),
+              child: GestureDetector(
+                // onTap: () {
+                //   Navigator.push(
+                //       context, MaterialPageRoute(builder: (_) => HomePage()));
+                // },
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: const Center(
+                    child: Text(
+                      "See What You Can Download",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+        Spacer()
+      ],
     );
   }
 }
--- lib/screens/quick_laugh.dart
+++ lib/screens/quick_laugh.dart
@@ -1,12 +1,85 @@
 import 'package:flutter/material.dart';
+import '../components/json/coming_soon.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class QuickLaughScreen extends StatelessWidget {
   const QuickLaughScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return SingleChildScrollView(
+      child: Column(
+        children: List.generate(comingSoonJson.length, (index) {
+
+          return Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: AssetImage(comingSoonJson[index]['img']),
+                  fit: BoxFit.cover,
+                )),
+              ),
+              Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Column(
+                          children: const [
+                            Icon(
+                              FontAwesomeIcons.faceLaughBeam,
+                              size: 28,
+                            ),
+                            SizedBox(height: 5),
+                            Text('91.1k')
+                          ],
+                        ),
+                        const SizedBox(height: 15),
+                        Column(
+                          children: const [
+                            Icon(
+                              Icons.add,
+                              size: 28,
+                            ),
+                            SizedBox(height: 5),
+                            Text('My List')
+                          ],
+                        ),
+                        const SizedBox(height: 15),
+                        Column(
+                          children: const [
+                            Icon(
+                              FontAwesomeIcons.paperPlane,
+                              size: 28,
+                            ),
+                            SizedBox(height: 5),
+                            Text('1.24k')
+                          ],
+                        ),
+                        const SizedBox(height: 15),
+                        Column(
+                          children: const [
+                            Icon(
+                              FontAwesomeIcons.play,
+                              size: 28,
+                            ),
+                            SizedBox(height: 5),
+                            Text('Play')
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                      ],
+                    ),
+                  )),
+            ],
+          );
+        }),
+      ),
     );
   }
 }
